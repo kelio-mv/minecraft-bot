77f0ae541e043c771e7cd5ee4c2ddf19072737c3
kelio-mv <keliojosue2@gmail.com>
2024-04-24 23:28:09 -0300

    Updates

--- pvp-bot-v2.js
+++ pvp-bot-v2.js
@@ -2,15 +2,16 @@ import mineflayerPathfinder from "mineflayer-pathfinder";
 const { pathfinder, Movements, goals } = mineflayerPathfinder;
 
 function pvpBotV2(bot) {
-  const reach = 3;
-  let opponent;
+  const followRange = 3;
+  const attackRange = 3;
+  let target;
   let fight;
 
   function startFollowing() {
     /* When using a range of 3, the bot somehow gets better at PvP, giving more knockback, getting
     more hits and automatically doing s-tap. However the bot may stop moving prematurely, if its
-    opponent is in a different Y coordinate. */
-    bot.pathfinder.setGoal(new goals.GoalFollow(opponent, reach));
+    target is in a different Y coordinate. */
+    bot.pathfinder.setGoal(new goals.GoalFollow(target, followRange));
   }
 
   function stopFollowing() {
@@ -31,10 +32,10 @@ function pvpBotV2(bot) {
   });
 
   bot.on("entitySpawn", (entity) => {
-    /* Save the opponent entity as soon as it spawns, and start following it, if it is supposed to
+    /* Save the target entity as soon as it spawns, and start following it, if it is supposed to
     fight it */
-    if (entity.username === "bot_v1") {
-      opponent = entity;
+    if (entity.username === "bot_yt") {
+      target = entity;
       if (fight) {
         startFollowing();
       }
@@ -46,7 +47,7 @@ function pvpBotV2(bot) {
     spawned, start following it */
     if (message === "fight") {
       fight = true;
-      if (opponent) {
+      if (target) {
         startFollowing();
       }
     } else if (message === "stop") {
@@ -56,16 +57,16 @@ function pvpBotV2(bot) {
   });
 
   bot.on("physicsTick", () => {
-    /* Don't run if the bot is not supposed to fight or if the opponent didn't spawn or load. */
-    if (!fight || !opponent) return;
+    /* Don't run if the bot is not supposed to fight or if the target didn't spawn or load. */
+    if (!fight || !target) return;
     /* Manual sprint */
     bot.setControlState("sprint", true);
     /* The calculation that determines if the player can hit another is based on hit boxes and
     slightly different than player's actual distance. A legit player might hit around 3.3 and
     3.4 blocks of distance if we consider the real distance. However, prefer using 3 to make sure
     your hits are legit and avoid getting banned. */
-    if (bot.entity.position.distanceTo(opponent.position) <= reach) {
-      bot.attack(opponent);
+    if (bot.entity.position.distanceTo(target.position) <= attackRange) {
+      bot.attack(target);
     }
   });
 
@@ -75,7 +76,7 @@ function pvpBotV2(bot) {
   });
 
   bot.on("death", () => {
-    /* Stop following opponent as soon as you die to prevent high CPU usage bug. */
+    /* Stop following the target as soon as you die to prevent high CPU usage bug. */
     stopFollowing();
   });
 }

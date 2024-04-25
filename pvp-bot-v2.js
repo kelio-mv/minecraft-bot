573b38f91ef63b597320714bf0b3485237851644
kelio-mv <keliojosue2@gmail.com>
2024-04-24 21:50:24 -0300

    Documentation

--- pvp-bot-v2.js
+++ pvp-bot-v2.js
@@ -14,18 +14,25 @@ function pvpBotV2(bot) {
   }
 
   function stopFollowing() {
+    /* If the bot is trying to follow an entity after killing it, using bot.pathfind.stop() won't
+    stop it. Use bot.pathfinder.setGoal(null) instead. */
     bot.pathfinder.setGoal(null);
   }
 
   bot.loadPlugin(pathfinder);
 
   bot.once("spawn", () => {
+    /* By default the bot can sprint but it makes weird moves when it should be walking straight.
+    Enabling allowFreeMotion solves this problem, but on the other hand, i need to manually sprint
+    on every physicsTick */
     const movements = new Movements(bot);
     movements.allowFreeMotion = true;
     bot.pathfinder.setMovements(movements);
   });
 
   bot.on("entitySpawn", (entity) => {
+    /* Save the opponent entity as soon as it spawns, and start following it, if it is supposed to
+    fight it */
     if (entity.username === "bot_v1") {
       opponent = entity;
       if (fight) {
@@ -35,6 +42,8 @@ function pvpBotV2(bot) {
   });
 
   bot.on("chat", (username, message) => {
+    /* Set fight to true to start following it when the entity spawns or is loaded. If it's already
+    spawned, start following it */
     if (message === "fight") {
       fight = true;
       if (opponent) {
@@ -47,20 +56,28 @@ function pvpBotV2(bot) {
   });
 
   bot.on("physicsTick", () => {
+    /* Don't run if the bot is not supposed to fight or if the opponent didn't spawn or load. */
     if (!fight || !opponent) return;
-
+    /* Manual sprint */
     bot.setControlState("sprint", true);
-
+    /* The calculation that determines if the player can hit another is based on hit boxes and
+    slightly different than player's actual distance. A legit player might hit around 3.3 and
+    3.4 blocks of distance if we consider the real distance. However, prefer using 3 to make sure
+    your hits are legit and avoid getting banned. */
     if (bot.entity.position.distanceTo(opponent.position) <= reach) {
       bot.attack(opponent);
     }
   });
 
   bot.on("goal_reached", () => {
+    /* If we set another goal in this event handler, it won't work. Use setTimeout to do that. */
     setTimeout(startFollowing);
   });
 
-  bot.on("death", stopFollowing);
+  bot.on("death", () => {
+    /* Stop following opponent as soon as you die to prevent high CPU usage bug. */
+    stopFollowing();
+  });
 }
 
 export default pvpBotV2;
